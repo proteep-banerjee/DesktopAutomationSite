2ec1c8a7d61062def386796dc44d4b58438203a8
pankaj.rawat <pankajazy@gmail.com>
2017-10-09 13:31:43 +0530

    updating tc

--- src/com/fabhotels/desktopsite/pageobjects/ListingPage.java
+++ src/com/fabhotels/desktopsite/pageobjects/ListingPage.java
@@ -7,6 +7,7 @@ import java.text.ParseException;
 import java.util.Iterator;
 import java.util.LinkedHashMap;
 import java.util.Map;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -22,7 +23,6 @@ public class ListingPage {
 	int hotelsCountUnderListPage = 1;
 	int cityCount = 1;
 	int randomCity_count = 1;
-	SoftAssert s_assert = new SoftAssert();
 
 	public static final By location_Txt = By.id("autocomplete-location");
 
@@ -54,12 +54,6 @@ public class ListingPage {
 	public static final By homePageSearchBox_WE = By.xpath("//input[@name='locationsearch']");
 
 	public static final String noPropertyFound_Msg = "We couldn’t find hotels matching your search criteria";
-
-	public ListingPage(WebDriver driver, GenericFunctions generic) {
-		this.driver = driver;
-		this.generic = generic;
-	}
-
 	public static final By noOfRooms_DD = By.xpath("//div[@class='custom-value-content clearfix']");
 
 	public static final By cityName_Lbl = By.xpath("//div[@class='hotel-info']/input");
@@ -92,7 +86,7 @@ public class ListingPage {
 	public static final By popCloseButton_Btn = By.xpath("//button[contains(@class,'vouchure_close')]");
 	public static final By hotelListPic_WE = By.xpath("//div[@class='hotel-list-pic']");
 	public static final By hotelListName_WE = By.xpath("//div[@class='hotel-list-desc-above']//h3");
-	public static final By hotelBook_Btn = By.xpath("//a[contains(text(),'Book Now')]");
+	public static final By hotelBook_Btn = By.xpath("//a[contains(text(),' Book Now')]");
 	public static final By allCities_dropDown = By.xpath("//a[@class='other-cities-icon']");
 	public static final By city_dropDown = By.xpath("//div[@class='other-cities-dropdown']//li/a");
 	public static final By mainLogo_WE = By.xpath("//div[@class='main-logo']");
@@ -104,11 +98,13 @@ public class ListingPage {
 																						// home
 																						// and
 																						// search
+	public static final By goToHomePage_Btn = By.xpath("//a[text()=' Go to Home Page ']");
 	public static final By searchResultContainer_WE = By.xpath("//div[@class='container search-result-page']");
 	public static final By nearByProperty_nav = By.xpath("//a[@class='flex-next']");
 	public static final By footer_stayUpdated = By.xpath("//div[@class='footer_row']//div[@class='stay_update']");
 	public static final By findFabHotel_Btn = By
 			.xpath("//*[contains(text(), ' Find FabHotels ') or contains(@value ,'Find FabHotels')]");
+	public static final By homePage_mainTitle_lbl = By.xpath("//div[contains(text(),'India’s Best')]");
 	public static String roomsNumber_WE = "//div[@class='custom-value-dropdown']//li/a[contains(text(),'";// Append
 	public static String bookingNow_Lbl = "(//div[@class='booking-now'])";
 	public static String nearBy_txt = "Explore more hotels near ";
@@ -118,7 +114,7 @@ public class ListingPage {
 	public static String nearByHotelImg_img = "//ul[@id='more_properties_content']//img";
 	public static final By cityErrorMsg_WE = By.xpath("//div[@id='cityErrorMsg']");
 	public static String cityErrorMsg_txt = "Please tell us where you are headed!";
-	public static String noHotelFoundMsg_txt = "//h2[contains(text(),' We couldn’t find hotels matching ')]";
+	public static final String noHotelFoundMsg_txt = "//h2[contains(text(),' We couldn’t find hotels matching ')]";
 
 	public static String locality_name = "Sarojini Nagar";
 
@@ -126,19 +122,17 @@ public class ListingPage {
 	public static final By hotelName_Lbl = By.xpath("//h1");
 	public static final By price_Lbl = By.xpath("//div[@class='select-room-price']//strong");
 
+	public ListingPage(WebDriver driver, GenericFunctions generic) {
+		this.driver = driver;
+		this.generic = generic;
+	}
+
 	public String getText_resultsCountText_Lbl() {
 		return generic.getText(resultsCountText_Lbl);
 	}
-	
-	public String getText_checkIn_Date_WE() {
-		return generic.getValue(checkIn_Date_WE);
-	}
-	
-	public String getText_checkOut_Date_WE() {
-		return generic.getValue(checkOut_Date_WE);
-	}
 
 	public void check_Hotels_count() {
+		SoftAssert s_assert = new SoftAssert();
 		for (int j = 1; j < 6; j++) {
 			System.out.println("Current city :" + iterate_all_Cities(randomCity_count));
 			int hotel_num = Integer.parseInt(getText_NoofHotelFound());
@@ -149,6 +143,7 @@ public class ListingPage {
 	}
 
 	public void compare_Rack_Price() {
+		SoftAssert s_assert = new SoftAssert();
 		for (int j = 1; j < 6; j++) {
 			System.out.println("Current city :" + iterate_all_Cities(randomCity_count));
 			String price, rack, hotelName = "";
@@ -176,6 +171,7 @@ public class ListingPage {
 	}
 
 	public void check_All_Hotel_links() {
+		SoftAssert s_assert = new SoftAssert();
 		for (int j = 1; j < 6; j++) {
 			System.out.println("Current city :" + iterate_all_Cities(randomCity_count));
 			generic.setImplicitWaitInSeconds(2);
@@ -192,6 +188,7 @@ public class ListingPage {
 	}
 
 	public void check_review() {
+		SoftAssert s_assert = new SoftAssert();
 		for (int j = 1; j < 5; j++) {
 			String cityName = iterate_all_Cities(randomCity_count);
 			System.out.println("Current city :" + cityName);
@@ -213,17 +210,17 @@ public class ListingPage {
 		String name_of_city = generic.getText(By.xpath("(//ul[@class='clearfix']/li/a)[" + i + "]"));
 		cityCount = generic.getSize(city_dropDown);
 		generic.click(By.xpath("(//ul[@class='clearfix']/li/a)[" + i + "]"));
+		generic.goToSleep(2000);
 		randomCity_count = generic.getRandomNumberBetween(1, cityCount);
 		hotelsCountUnderListPage = generic.getSize(hotelList_WE);
-		generic.goToSleep(2000);
 		return name_of_city;
 	}
 
 	public void all_Cities_names() {
+		SoftAssert s_assert = new SoftAssert();
 		for (int j = 1; j < 6; j++) {
 			String cityName = iterate_all_Cities(randomCity_count);
 			System.out.println("Current city :" + cityName);
-			System.out.println("dededed" + generic.getValue(cityName_Lbl).trim());
 			s_assert.assertEquals(generic.getValue(cityName_Lbl).trim(), cityName,
 					"Hotel info name is not matching with picked city.");
 			if (generic.isVisible(resultsCountText_Lbl)) {
@@ -237,6 +234,7 @@ public class ListingPage {
 	}
 
 	public void check_ImageAndLandmark() {
+		SoftAssert s_assert = new SoftAssert();
 		for (int j = 1; j < 6; j++) {
 			String cityName = iterate_all_Cities(randomCity_count);
 			System.out.println("Current city :" + cityName);
@@ -265,18 +263,20 @@ public class ListingPage {
 	}
 
 	// TO check NearBy property.
-	public void searchOnListPage() {
-		searchBox(searchBox_WE, locality_name, 4);
-		generic.goToSleep(1000);
+	public void verifyNearByPropertyOnListPage() {
+		SoftAssert s_assert = new SoftAssert();
 		int i = 1;
-		Assert.assertTrue(generic.isVisible(nearByHotelName_lbl), "Near By Property is Coming!!!.");
+		generic.goToSleep(1000);
+		generic.scrollToElement(nearByProperty_nav, false);
+		Assert.assertTrue(generic.isVisible("(" + nearByHotelName_lbl + ")" + "[" + i + "]"),
+				"Near By Property is Not Coming!!!.");
+		generic.goToSleep(2000);
 		while (!driver.findElement(lastHotelNearBy_WE).isDisplayed()) {
-			generic.scrollToElement(nearByProperty_nav, false);
 			if (driver.findElement(By.xpath("(" + nearByHotelName_lbl + ")" + "[" + i + "]")).isDisplayed()) {
 				String hotelName = generic.getText("(" + nearByHotelName_lbl + ")" + "[" + i + "]");
 				System.out.println(i + ". Hotle ," + hotelName);
 				s_assert.assertTrue(generic.isVisible("(" + totalNearByProperty_WE + ")" + "[" + i + "]"),
-						"Near By Hotels are not comming");
+						"0 Near By Hotels are comming");
 				s_assert.assertTrue(
 						generic.isVisible(
 								"(" + totalNearByProperty_WE + ")" + "[" + i + "]" + "//span[@class='new-price']"),
@@ -290,7 +290,7 @@ public class ListingPage {
 
 				s_assert.assertTrue(generic.isVisible("(" + nearByHotelReview_lbl + ")" + "[" + i + "]"),
 						"Hotel review is not comming for :" + hotelName);
-				
+
 				String imgSrc = generic.getAttributeValue(By.xpath("(" + nearByHotelImg_img + ")" + "[" + i + "]"),
 						"src");
 				String imageName = imgSrc.split("/")[imgSrc.split("/").length - 1];
@@ -299,10 +299,16 @@ public class ListingPage {
 			} else {
 				generic.click(nearByProperty_nav);
 			}
-			
 		}
-		s_assert.assertTrue(driver.findElement(By.xpath(totalNearByProperty_WE)).isDisplayed(),
+		s_assert.assertTrue(driver.findElement(footer_stayUpdated).isDisplayed(),
 				"On Footer stay updated is not comming.");
+		// Clicking on last nearby hotel book button
+		String lastNearByHotelName = generic.getText("(" + nearByHotelName_lbl + ")" + "[" + i + "]");
+		generic.click("(" + nearByBook_btn + ")" + "[" + i + "]");
+		String hotelName_DetailsPage = generic.getText(hotelName_Lbl);
+		generic.goToSleep(2000);
+		s_assert.assertEquals(hotelName_DetailsPage, lastNearByHotelName,
+				"Navigating from near by property hotel to Details page is failing...");
 		s_assert.assertAll();
 	}
 
@@ -318,80 +324,94 @@ public class ListingPage {
 
 	}
 
-	public void searchBox(By searchBox, String searchText, int roomNo) {
-		generic.fill(searchBox, searchText);
-		// WithDates
-		if (roomNo != 0) {
-			Calendar cal = new Calendar(driver, generic);
+	public void performSearch(String searchText, String checkIn, String checkOut, String noofRooms) {
+		generic.fill(searchBox_WE, searchText);
+		Calendar cal = new Calendar(driver, generic);
+
+		if (checkIn.length() > 0 && checkOut.length() > 0) {
 			try {
-				cal.Select_CheckIn_CheckOut_Date_SecondCalendaronDEtailsPage_WE(cal.dateWithDifferentFormat("dd MMMM uuuu", 1),
-						cal.dateWithDifferentFormat("dd MMMM uuuu", 3));
+				cal.Select_CheckIn_CheckOut_Date_Calendar_WE(checkIn, checkOut);
 			} catch (ParseException e) {
 				e.printStackTrace();
 			}
-			generic.performMouseHover(noOfRooms_DD);
-			generic.click(roomsNumber_WE + roomNo + "')]");
-			generic.click(findFabHotel_Btn);
-			generic.goToSleep(4000);
-			generic.isVisible(searchResultContainer_WE);
-			s_assert.assertTrue(generic.getText(resultsCountText_Lbl).contains(searchText),
-					"Searched result not Appropriate on ListPage");
-			// DateLess
-		} else {
-			generic.click(findFabHotel_Btn);
-			generic.goToSleep(4000);
-
 		}
+
+		if (noofRooms.length() > 0) {
+			generic.performMouseHover(noOfRooms_DD);
+			generic.click(roomsNumber_WE + noofRooms + "')]");
+		}
+
+		generic.click(findFabHotel_Btn);
+		generic.goToSleep(1000);
 	}
 
+	// Func to get url parameters.
 	public Map<String, String> getQueryURL() {
 		Map<String, String> query_pairs = new LinkedHashMap<>();
 		String result = "";
 		String query = "";
+		int occupancy_count = 0;
 		try {
 			result = java.net.URLDecoder.decode(generic.getCurrentUrl(), "UTF-8");
 			query = new URL(result).getQuery();
 		} catch (UnsupportedEncodingException e) {
 			e.printStackTrace();
 		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		String[] pairs = query.split("&");
 		System.out.println("  PARAMETERS: " + "  VALUES:  ");
 		for (String pair : pairs) {
 			int idx = pair.indexOf("=");
-			query_pairs.put(pair.substring(0, idx), pair.substring(idx + 1));
+			if (pair.substring(0, idx).equals("occupancy[]")) {
+				occupancy_count++;
+			} else {
+				query_pairs.put(pair.substring(0, idx), pair.substring(idx + 1));
+			}
 			System.out.println(pair.substring(0, idx) + "\t" + pair.substring(idx + 1));
 		}
-		// System.out.println("checkoutDate: " +
-		// query_pairs.get("location_text"));
+		// Storing occupancy count on Map as occupancy property is redundant.
+		// USE occupancy as a key to get the occupancy count.
+		query_pairs.put("occupancy", String.valueOf(occupancy_count));
 		return query_pairs;
 
 	}
 
 	public void checkURL() {
-		searchBox(searchBox_WE, locality_name, 4);
+		SoftAssert s_assert = new SoftAssert();
 		Map<String, String> parameters = getQueryURL();
 		s_assert.assertTrue(parameters.get("locationsearch").contains(locality_name),
-				"Location attribute is not correct.");
+				"Location parameter is not correct.in the url");
+		s_assert.assertTrue(parameters.get("locality_text").contains(locality_name),
+				"Location parameter is not correct in the url.");
+		s_assert.assertEquals("4", parameters.get("occupancy"),
+				"Occupancy is not Matching with default value. i.e 1 OR is broken");
+		s_assert.assertTrue(parameters.get("nearcity").contains("New Delhi"),
+				"Near city parameter is not correct.in the url");
+		s_assert.assertAll();
 	}
 
 	public void assertSearchError() {
-		searchBox(searchBox_WE, "", 1);
+		SoftAssert s_assert = new SoftAssert();
 		s_assert.assertEquals(generic.getText(cityErrorMsg_WE), cityErrorMsg_txt,
 				"List Box errror warning is not comming.");
+		s_assert.assertAll();
 	}
 
 	public void No_Hotelsfound() {
-		searchBox(searchBox_WE, "*", 0);
-		s_assert.assertTrue(generic.isVisible(noHotelFoundMsg_txt), "Error warning is functionalty is broken");
-
+		SoftAssert s_assert = new SoftAssert();
+		s_assert.assertTrue(generic.isVisible(noHotelFoundMsg_txt), "Error warning functionalty is broken");
+		s_assert.assertTrue(generic.isVisible(goToHomePage_Btn), "Go to Home Page is not comming");
+		generic.click(goToHomePage_Btn);
+		s_assert.assertTrue(generic.isVisible(homePage_mainTitle_lbl), "Go to Home Page is Not Working.");
+		s_assert.assertAll();
 	}
 
 	public void nearByHotelslistPage() {
+		SoftAssert s_assert = new SoftAssert();
 		s_assert.assertTrue(generic.findElement(nearByHotel_Lbl).getText().contentEquals(nearBy_txt + locality_name),
 				"Near By Hotel is broken on List Page");
+		s_assert.assertAll();
 	}
 
 	public String getText_NoofHotelFound() {
@@ -399,8 +419,7 @@ public class ListingPage {
 	}
 
 	public void detailsPageLanding_VerifyWithDateAndRooms() {
-		// List Page
-		searchBox(searchBox_WE, locality_name, 4);
+		SoftAssert s_assert = new SoftAssert();
 		String date_ListPage = generic.getAttributeValue(checkIn_Date_WE, "value");
 		String price_ListPage = generic
 				.getText("(//div[@class='price'])" + "[" + 1 + "]" + "//span[@class='new-price']")
@@ -413,7 +432,7 @@ public class ListingPage {
 
 		// Clicking on BookNow of first hotel
 		generic.click(bookNow_FirstTuple_Lbl);
-		generic.goToSleep(5000);
+		generic.goToSleep(2000);
 
 		// Details Page
 		String date_DetailsPage = generic.getAttributeValue(checkIn_Date_WE, "value");
@@ -432,14 +451,12 @@ public class ListingPage {
 		s_assert.assertTrue(parametersDetails.get("rooms").contains("4"),
 				"Rooms parameters are not matching in Details Page's URL.");
 		generic.navigateToPreviousPage();
-		s_assert.assertTrue(generic.isVisible(hotelBook_Btn),
-				"Book Now Button is not comming on navigating back from Detials Page with Date case.");
+		s_assert.assertAll();
 
 	}
 
 	public void detailsPageLanding_VerifyWithDateLess() {
-		// List Page
-		searchBox(searchBox_WE, "New Delhi", 0);
+		SoftAssert s_assert = new SoftAssert();
 		String price_ListPage = generic
 				.getText("(//div[@class='price'])" + "[" + 1 + "]" + "//span[@class='new-price']")
 				.replaceAll("[^0-9.]", "");
@@ -453,13 +470,15 @@ public class ListingPage {
 
 		// Clicking on BookNow of first hotel
 		generic.click(bookNow_FirstTuple_Lbl);
-
+		generic.goToSleep(2000);
 		// Details Page
 		String hotelName_DetailsPage = generic.getText(hotelName_Lbl);
 		String price_DetailsPage = generic.getText(price_Lbl).replaceAll("[^0-9.]", "");
 		// Asserting Page
-		s_assert.assertEquals(hotelName_DetailsPage, hotelName_ListPage);
-		s_assert.assertEquals(price_DetailsPage, price_ListPage);
+		s_assert.assertEquals(hotelName_DetailsPage, hotelName_ListPage,
+				"Hotels Names are not matching on details and list page");
+		s_assert.assertEquals(price_DetailsPage, price_ListPage,
+				"Hotels Prices are not matching on details and list page");
 		Map<String, String> parametersDetails = getQueryURL();
 		String cityName_DetailsPage = parametersListPage.get("locationsearch");
 		// Asserting URL
@@ -470,8 +489,7 @@ public class ListingPage {
 				"rooms parameter is not 1 with dateless case on Details Page's URL.");
 		// Navigating Back
 		generic.navigateToPreviousPage();
-		s_assert.assertTrue(generic.isVisible(hotelBook_Btn),
-				"Book Now Button is not comming on navigating back from Detials Page with Date case.");
+		s_assert.assertAll();
 
 	}
 
@@ -479,9 +497,6 @@ public class ListingPage {
 		generic.click(singleProperty_WE);
 
 	}
-	
-
-	
 
 	public void click_Filters_WE() {
 		generic.click(filters_WE);
@@ -499,4 +514,11 @@ public class ListingPage {
 		generic.click(hotelName_FirstTuple_Lbl);
 	}
 
+	public String getText_checkIn_Date_WE() {
+		return generic.getValue(checkIn_Date_WE);
+	}
+
+	public String getText_checkOut_Date_WE() {
+		return generic.getValue(checkOut_Date_WE);
+	}
 }
--- src/com/fabhotels/desktopsite/testsuite/listingpage/ExecutionSuite_ListingPage.java
+++ src/com/fabhotels/desktopsite/testsuite/listingpage/ExecutionSuite_ListingPage.java
@@ -6,42 +6,43 @@ import java.util.List;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-import com.fabhotels.automationframework.xlsreader.Xls_Reader;
+
+import com.fabhotels.automationframework.genericfunctions.GenericFunctions;
 import com.fabhotels.desktopsite.pageobjects.Calendar;
 import com.fabhotels.desktopsite.pageobjects.ListingPage;
 import com.fabhotels.desktopsite.utils.Config;
 import com.fabhotels.desktopsite.utils.UrlProvider;
 
 public class ExecutionSuite_ListingPage extends Config {
-	String Sheetname;
-	Xls_Reader datatable;
+
+	ListingPage listingPage;
 
 	@BeforeTest
 	public void beforeTest() {
+		generic = new GenericFunctions(driver);
 		driver = generic.startDriver(Driver_Type);
-		generic.maximize();
+		listingPage = new ListingPage(driver, generic);
 	}
 
 	@BeforeMethod
 	public void beforeMethod() {
 		driver.manage().deleteAllCookies();
+		generic.loadURL(UrlProvider.getListingPageUrl());
+		generic.handlePopUPTimer(ListingPage.popCloseButton_Btn);
 	}
 
 	@Test
 	public void TC_ValidateVisibility_FeatureBox_ListPage() {
-		generic.loadURL(UrlProvider.getListingPageUrl());
 		Assert.assertTrue(generic.isVisible(ListingPage.featureBox_WE), "Feature box not visible on list page !!");
-
 	}
 
 	@Test
 	public void TC_ValidateFunctionality_Filters() {
-		generic.loadURL(UrlProvider.getListingPageUrl());
-		ListingPage listingPage = new ListingPage(driver, generic);
 		List<WebElement> filters = generic.findElements(ListingPage.filters_WE);
 		String firstFilter = filters.get(0).findElement(By.tagName("span")).getText();
 		listingPage.click_Filters_WE();
@@ -52,53 +53,39 @@ public class ExecutionSuite_ListingPage extends Config {
 
 	@Test
 	public void TC_CheckTotal_HotelCount_List() {
-		ListingPage listingPage = new ListingPage(driver, generic);
-		generic.loadURL(UrlProvider.getListingPageUrl());
 		listingPage.check_Hotels_count();
 	}
 
 	@Test
 	public void TC_CheckRackPrice_List() {
-		generic.loadURL(UrlProvider.getListingPageUrl());
-		ListingPage listingPage = new ListingPage(driver, generic);
 		listingPage.compare_Rack_Price();
 	}
 
 	@Test
 	public void TC_Three_hotels_links() {
-		generic.loadURL(UrlProvider.getListingPageUrl());
-		ListingPage listingPage = new ListingPage(driver, generic);
 		listingPage.check_All_Hotel_links();
 	}
 
 	@Test
 	public void TC_Review_listPage() {
-		generic.loadURL(UrlProvider.getListingPageUrl());
-		ListingPage listingPage = new ListingPage(driver, generic);
 		listingPage.check_review();
 	}
 
 	@Test
 	public void TC_CityDropdown_listPage() {
-		generic.loadURL(UrlProvider.getHomePageUrl());
-		ListingPage listingPage = new ListingPage(driver, generic);
 		listingPage.all_Cities_names();
 	}
 
 	@Test
 	public void TC_ImageandLandmark_listPage() {
-		generic.loadURL(UrlProvider.getHomePageUrl());
-		ListingPage listingPage = new ListingPage(driver, generic);
 		listingPage.check_ImageAndLandmark();
 	}
 
 	@Test
 	public void TC_HotelInfoContainer_listPage() throws ParseException {
-		generic.loadURL(UrlProvider.getListingPageUrl());
-		generic.dissMissPopUPTimer(ListingPage.popCloseButton_Btn);
-		ListingPage listingPage = new ListingPage(driver, generic);
 		Calendar cal = new Calendar(driver, generic);
-		cal.Select_CheckIn_CheckOut_Date_Calendar_WE(cal.dateWithDifferentFormat("dd MMMM uuuu",1),cal.dateWithDifferentFormat("dd MMMM uuuu", 3));
+		cal.Select_CheckIn_CheckOut_Date_Calendar_WE(cal.dateWithDifferentFormat("dd MMMM uuuu", 1),
+				cal.dateWithDifferentFormat("dd MMMM uuuu", 3));
 		listingPage.selectRooms();
 		List<WebElement> list = generic.findElements(ListingPage.filters_WE);
 		for (WebElement temp : list) {
@@ -107,8 +94,65 @@ public class ExecutionSuite_ListingPage extends Config {
 		}
 	}
 
+	@Test
+	public void TC_NearByHotels_listPage() {
+		listingPage.performSearch(ListingPage.locality_name, "", "", "4");
+		Assert.assertTrue(generic.isVisible(ListingPage.searchResultContainer_WE),
+				"Search results not visible on performing Search !!");
+		Assert.assertTrue(generic.getText(ListingPage.resultsCountText_Lbl).contains(ListingPage.locality_name),
+				"Search results not Appropriate on ListPage !!");
+		listingPage.verifyNearByPropertyOnListPage();
+	}
+
+	@Test
+	public void TC_SearchBox_listPage() {
+		listingPage.performSearch("", "", "", "1");
+		listingPage.assertSearchError();
+	}
+
+	@Test
+	public void TC_checkURL_listPage() {
+		listingPage.performSearch(ListingPage.locality_name, "", "", "4");
+		Assert.assertTrue(generic.isVisible(ListingPage.searchResultContainer_WE),
+				"Search results not visible on performing Search !!");
+		Assert.assertTrue(generic.getText(ListingPage.resultsCountText_Lbl).contains(ListingPage.locality_name),
+				"Search results not Appropriate on ListPage !!");
+		listingPage.detailsPageLanding_VerifyWithDateAndRooms();
+		listingPage.checkURL();
+	}
+
+	@Test
+	public void TC_No_Hotelsfound_listPage() {
+		generic.loadURL(UrlProvider.getHomePageUrl());
+		listingPage.performSearch("*", "", "", "");
+		listingPage.No_Hotelsfound();
+	}
+
+	@Test
+	public void TC_DetailsPageLanding_Dates_listPage() {
+		listingPage.performSearch(ListingPage.locality_name, "", "", "4");
+		Assert.assertTrue(generic.isVisible(ListingPage.searchResultContainer_WE),
+				"Search results not visible on performing Search !!");
+		Assert.assertTrue(generic.getText(ListingPage.resultsCountText_Lbl).contains(ListingPage.locality_name),
+				"Search results not Appropriate on ListPage !!");
+		listingPage.detailsPageLanding_VerifyWithDateAndRooms();
+	}
+
+	@Test
+	public void TC_DetailsPageLanding_DateLess_listPage() {
+		listingPage.performSearch("Bangalore", "", "", "");
+		Assert.assertTrue(generic.isVisible(ListingPage.searchResultContainer_WE),
+				"Search results not visible on performing Search !!");
+		listingPage.detailsPageLanding_VerifyWithDateLess();
+	}
+
+	@AfterMethod
+	public void afterMethod() {
+
+	}
+
 	@AfterTest
-	public void afterTest() {
+	public void aftertest() {
 		driver.quit();
 	}
 }
